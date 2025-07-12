acdba2ddbdb7c2ed14f298e9216492fb2a616493
William Ramirez <william.ramirez.paucar@udla.edu.ec>
2025-07-12 12:13:36 -0500

    Crear modelo para logs

---
+++ Models/LogEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArriendoPocketApp.Models
+{
+    internal class LogEntry
+    {
+        public int Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Level { get; set; }
+        public string Message { get; set; }
+        public string? Endpoint { get; set; }
+        public string? Payload { get; set; }
+        public string? UserId { get; set; }
+    }
+}
